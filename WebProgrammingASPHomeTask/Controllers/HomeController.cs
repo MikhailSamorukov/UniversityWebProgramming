dfcfa6f2f8d827d01e61eee63cd8554edfba731f
Mihail Samorukov <Misha_Sam@mail.ru>
2018-03-18 21:56:16 +0600

    finished

---
+++ WebProgrammingASPHomeTask/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProgrammingASPHomeTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Number4()
+        {
+            return View();
+        }
+
+        public ActionResult Number9()
+        {
+            return View();
+        }
+
+        public ActionResult Number14()
+        {
+            return View();
+        }
+        public ActionResult GreenPage()
+        {
+            return View();
+        }
+        public ActionResult Number19()
+        {
+            return View();
+        }
+        public ActionResult Number24()
+        {
+            return View();
+        }
+        public ActionResult Number29()
+        {
+            return View();
+        }
+        public ActionResult Number34()
+        {
+            return View();
+        }
+        public ActionResult Number39()
+        {
+            return View();
+        }
+        public ActionResult Number44()
+        {
+            return View();
+        }
+        public ActionResult Number49()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebProgrammingASPHomeTask/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebProgrammingASPHomeTask.MvcApplication" Language="C#" %>
